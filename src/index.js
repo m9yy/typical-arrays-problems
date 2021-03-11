f764e91b9039be5fe26e53ea63682ff2eb908e59
Sergey Larchenko <newm9yy2020@gmail.com>
2021-03-11 13:45:34 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+  };
+
+  return Math.min.apply(null, array);
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  return Math.max.apply(null, array);
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
+
 }
